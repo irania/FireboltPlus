52c4af10664f8a5ade461b0038766d173758d906
mh.saadatfar <mh.saadatfar@gmail.com>
2018-05-13 10:37:21 +0430

    - Pulse Test Updated.
    - Shower Test Updated.

--- app/src/main/java/com/Tirax/plasma/Test/TestPulse.java
+++ app/src/main/java/com/Tirax/plasma/Test/TestPulse.java
@@ -11,13 +11,19 @@ public class TestPulse {
     public void Run() {
         TestResult.setLog("Ready...");
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         DataProvider.setRegister(DataProvider.RPWR, (char) 50);
+        DataProvider.setRegister(DataProvider.RDPTH, (char) 75);
         DataProvider.setRegister(DataProvider.MTTI, (char) 4);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         DataProvider.setRegister(DataProvider.MTB1, (char) 1);
 
         try {
@@ -26,6 +32,9 @@ public class TestPulse {
             return;
         }
 
+        DataProvider.setRegister(DataProvider.RPWR, (char) 0);
+        DataProvider.setRegister(DataProvider.RDPTH, (char) 0);
+        DataProvider.setRegister(DataProvider.MTTI, (char) 0);
         TestResult.setLog("Test Done.");
     }
 }
--- app/src/main/java/com/Tirax/plasma/Test/TestShower.java
+++ app/src/main/java/com/Tirax/plasma/Test/TestShower.java
@@ -11,13 +11,20 @@ public class TestShower {
     public void Run() {
         TestResult.setLog("Ready...");
         try {
-            Thread.sleep(3000);
+            Thread.sleep(2500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         DataProvider.setRegister(DataProvider.RPWR, (char) 50);
+        DataProvider.setRegister(DataProvider.RDPTH, (char) 75);
         DataProvider.setRegister(DataProvider.MTTI, (char) 9);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         for (int i=0;i<15;i++) {
             TestResult.setLog("Running...  (Min: "+i+")");
@@ -31,6 +38,10 @@ public class TestShower {
                 if (Thread.interrupted()) return;
             }
         }
+
+        DataProvider.setRegister(DataProvider.RPWR, (char) 0);
+        DataProvider.setRegister(DataProvider.RDPTH, (char) 0);
+        DataProvider.setRegister(DataProvider.MTTI, (char) 0);
         TestResult.setLog("Test Done.");
     }
 }
